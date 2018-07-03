c867bb699f7e561b8f61f2fabc30882ce339e73d
Zachary Fine <zfine416@gmail.com>
2018-07-03 01:23:29 -0400

    remove all route

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
 } else {
   app.use('/', express.static(path.join(__dirname, 'public')));
 }
